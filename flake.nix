3e74c803112fc21abdc676bcf6280092630b9912
Antal Buss <antalbuss@fastmail.ca>
2025-09-15 22:19:11 -0600

    Update initial flake.nis

--- flake.nix
+++ flake.nix
@@ -1,39 +1,32 @@
 {
   description = "My Simple NixOS and Home Manager Configuration";
+
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    # You can access packages and modules from different nixpkgs revs at the
-    # same time. See 'unstable-packages' overlay in 'overlays/default.nix'.
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
-    # agenix.url = "github:ryantm/agenix";
-    # agenix.inputs.nixpkgs.follows = "nixpkgs";
-
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko = {
+        url = "github:nix-community/disko";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # nix-formatter-pack.url = "github:Gerschtli/nix-formatter-pack";
-    # nix-formatter-pack.inputs.nixpkgs.follows = "nixpkgs";
-
+    home-manager = {
+        url = "github:nix-community/home-manager/release-25.05"
+        inputs.nixpkgs.follows = "nixpkgs";;
+    };
+    
     # Hardware definition
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    # nix-colors.url = "github:misterio77/nix-colors";
-
+    # nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs =
-    # { self, nix-formatter-pack, nixpkgs, home-manager, ...} @ inputs:
     { self, nixpkgs, home-manager, ...} @ inputs:
     let
       inherit (self) outputs;
       # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-      stateVersion = "24.11";
+      stateVersion = "25.05";
 
       username = "abuss";
 
@@ -49,9 +42,8 @@
 
       nixosConfigurations = {
         # Workstations
-        #  - sudo nixos-rebuild switch --flake $HOME/.dotfiles/nix-config
-        #  - nix build .#nixosConfigurations.ripper.config.system.build.toplevel
-        # Test
+        #  - sudo nixos-rebuild switch --flake $HOME/.dotfiles/nix-config#testvm
+        # Test vm
         testvm = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs outputs stateVersion username;};
           modules = [ ./hosts/testvm/nixos/configuration.nix ];  
@@ -76,8 +68,8 @@
       # home-manager switch -b backup --flake $HOME/.dotfiles/nix-config
       # nix build .#homeConfigurations."abuss@nixosvm".activationPackage
       homeConfigurations = {
-        "abuss@testvm" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        "${abuss}@testvm" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
           extraSpecialArgs = {inherit inputs outputs stateVersion username;};
           modules = [ ./hosts/testvm/home-manager/home.nix ];
         };
@@ -101,19 +93,5 @@
         };
       };
 
-      # Devshell for bootstrapping; acessible via 'nix develop' or 'nix-shell' (legacy)
-      # devShells = forAllSystems (system:
-      #   let pkgs = nixpkgs.legacyPackages.${system};
-      #   in import ./shell.nix { inherit pkgs; }
-      # );
-
-      # Custom packages and modifications, exported as overlays
-      # overlays = import ./overlays { inherit inputs; };
-
-      # Custom packages; acessible via 'nix build', 'nix shell', etc
-      # packages = forAllSystems (system:
-      #   let pkgs = nixpkgs.legacyPackages.${system};
-      #   in import ./pkgs { inherit pkgs; }
-      # );
     };
 }

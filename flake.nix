1555727898992980db04f3cf731a98650fccc832
Antal A. Buss <abuss@users.noreply.github.com>
2025-09-16 01:08:06 -0600

    Update flake.nix missing ;

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
     # Home manager
     home-manager = {
-        url = "github:nix-community/home-manager/release-25.05"
+        url = "github:nix-community/home-manager/release-25.05";
         inputs.nixpkgs.follows = "nixpkgs";
     };
     
